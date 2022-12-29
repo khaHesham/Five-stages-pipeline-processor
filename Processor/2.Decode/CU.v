158205fc2885e3e3f9e6d379ea10953393da19aa
nadaaosman <84284180+nadaaosman@users.noreply.github.com>
2022-12-29 19:53:22 +0200

    cu is done

--- Processor/2.Decode/CU.v
+++ Processor/2.Decode/CU.v
@@ -1,5 +1,5 @@
-module Control_Unit (opcode,MEM_signals,EX_signals,WB_signals,flush);
-
+module Control_Unit (opcode,interrupt,inst_before_call,inter_state_before,ret_state_before,reti_state_before,f_d_buffer_enable,pc_enable,flush,jump_sel,MEM_signals,EX_signals,WB_signals,inter_state_after,ret_state_after,reti_state_after);
+    //opcodes
     localparam NOP =6'b000000;
     localparam SETC=6'b000001;
     localparam CLRC=6'b000010;
@@ -27,62 +27,389 @@ module Control_Unit (opcode,MEM_signals,EX_signals,WB_signals,flush);
     localparam CALL=6'b100101;
     localparam RET =6'b100110;
     localparam RETI=6'b100111;
+    //states
 
+    //interrupt
+    localparam NO_INTERRUPT =2'b00;
+    localparam PUSH_FLAGS =2'b01;
+    localparam PUSH_1 =2'b10;
+    //ret
+    localparam NO_RET  =3'b000;
+    localparam POP_1_RET =3'b001;
+    localparam POP_2_RET =3'b010;
+    localparam NOP1_RET =3'b011;
+    localparam NOP2_RET =3'b100;
+    //reti
+    localparam NO_RETI  =3'b000;
+    localparam POP_FLAGS =3'b001;
+    localparam POP_1_RETI =3'b010;
+    localparam POP_2_RETI =3'b011;
+    localparam NOP1_RETI =3'b100;
+    localparam NOP2_RETI =3'b101;
 
-
-
-
+    //inputs
     input [5:0] opcode;
-    output reg flush;
-    output reg [3:0] MEM_signals; //memRead(1), memWrite(1), memAddress(1), memData(1)
-    output reg [6:0] EX_signals;  //ALUop(4+1enable), shamSelt(1), flag_en
-    output reg [2:0] WB_signals;  //regWrite(1), WBsel(2)
+    input interrupt,inst_before_call;
+    //state_before
+    input [1:0] inter_state_before;
+    input [2:0] ret_state_before,reti_state_before;
 
+   //for fetch and flush also for decode 
+    output reg f_d_buffer_enable,pc_enable,flush;
+    output reg [1:0]jump_sel;
+    //for execute , memo and WB
+    output reg [6:0] MEM_signals; //memRead(1), memWrite(1), memAddress(2), memData(3)
+    output reg [13:0] EX_signals;  //branch(3),call(1),alu_op(4),rsrc_sel(2),rdst_sel(2),flags_enb(1),alu_en(1)
+    output reg [5:0] WB_signals;  //sp_wr(1), flags_wb(1),wb_sel(1),pop_l_h(2),regwrite(1)
+    
+    //state_after
+    output reg [1:0] inter_state_after;
+    output reg [2:0] ret_state_after,reti_state_after;
    
     always @(*) begin
-        
+        if( |inter_state_before || interrupt)  //INTERRUPT
+        begin
+          ret_state_after=3'b000;
+          reti_state_after=3'b000;
+        case(inter_state_before)   //push_flags
+         NO_INTERRUPT:begin
+         inter_state_after=2'b01;
+         f_d_buffer_enable=0;
+         pc_enable=0;
+         flush=0;
+         jump_sel=2'b00;
+         EX_signals=14'b00000001000001;
+         MEM_signals=7'b0111010;
+         WB_signals=6'b100000;
+         end
+         PUSH_FLAGS:begin            //push_1
+        inter_state_after=2'b10;
+        f_d_buffer_enable=0;
+        pc_enable=1;
+        flush=0;
+        jump_sel=2'b10;
+        EX_signals=14'b00000001000001;
+        MEM_signals=7'b0111100;
+        WB_signals=6'b100000;
+         end
+         PUSH_1:begin                  //push_2
+        inter_state_after=2'b00;
+        f_d_buffer_enable=1;
+        pc_enable=1;
+        flush=0;
+        jump_sel=2'b00;
+        EX_signals=14'b00000001000001;
+        MEM_signals=7'b0111011;
+        WB_signals=6'b100000;
+         end
+        endcase
+        end
+        else if( |ret_state_before || opcode==RET)   //RET
+        begin
+        reti_state_after=3'b000;   
+        inter_state_after=2'b00; 
+        case(ret_state_before)
+        NO_RET:begin      //pop_1
+        ret_state_after=3'b001;
+        f_d_buffer_enable=0;
+        pc_enable=0;
+        flush=0;
+        jump_sel=2'b00;
+        EX_signals=14'b00000000000001;
+        MEM_signals=7'b1010000;
+        WB_signals=6'b101101;
+       end
+       POP_1_RET:begin    //pop_2
+        ret_state_after=3'b010;
+        f_d_buffer_enable=0;
+        pc_enable=0;
+        flush=0;
+        jump_sel=2'b00;
+        EX_signals=14'b00000000000001;
+        MEM_signals=7'b1010000;
+        WB_signals=6'b101111;
+       end
+       POP_2_RET:begin     //NOP1
+        ret_state_after=3'b011;
+        f_d_buffer_enable=0;
+        pc_enable=0;
+        flush=0;
+        jump_sel=2'b00;
+        EX_signals=14'b00000000000000;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000000;
+       end
+       NOP1_RET:begin   //NOP2
+        ret_state_after=3'b100;
+        f_d_buffer_enable=0;
+        pc_enable=1;
+        flush=0;
+        jump_sel=2'b11;
+        EX_signals=14'b00000000000000;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000000;
+       end
+       NOP2_RET:begin    //NOP3
+        ret_state_after=3'b000;
+        f_d_buffer_enable=1;
+        pc_enable=1;
+        flush=0;
+        jump_sel=2'b00;
+        EX_signals=14'b00000000000000;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000000;
+       end
+        endcase
+        end
+        else if( |reti_state_before || opcode==RETI)   //RETI
+        begin
+        ret_state_after=3'b000;
+        inter_state_after=2'b00;
+         case(reti_state_before)
+        NO_RETI:begin      //pop_flags
+        reti_state_after=3'b001;
+        f_d_buffer_enable=0;
+        pc_enable=0;
+        flush=0;
+        jump_sel=2'b00;
+        EX_signals=14'b00000000000001;
+        MEM_signals=7'b1010000;
+        WB_signals=6'b111000;
+        end
+        POP_FLAGS:begin    //pop_1
+        reti_state_after=3'b010;
+        f_d_buffer_enable=0;
+        pc_enable=0;
+        flush=0;
+        jump_sel=2'b00;
+        EX_signals=14'b00000000000001;
+        MEM_signals=7'b1010000;
+        WB_signals=6'b101101;
+        end
+        POP_1_RETI:begin   //pop_2
+        reti_state_after=3'b011;
+        f_d_buffer_enable=0;
+        pc_enable=0;
+        flush=0;
+        jump_sel=2'b00;
+        EX_signals=14'b00000000000001;
+        MEM_signals=7'b1010000;
+        WB_signals=6'b101111;
+        end
+        POP_2_RETI:begin   //   NOP1
+        reti_state_after=3'b100;
+        f_d_buffer_enable=0;
+        pc_enable=0;
+        flush=0;
+        jump_sel=2'b00;
+        EX_signals=14'b00000000000000;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000000;
+        end
+        NOP1_RETI:begin    //NOP2
+        reti_state_after=3'b101;
+        f_d_buffer_enable=0;
+        pc_enable=1;
+        flush=0;
+        jump_sel=2'b11;
+        EX_signals=14'b00000000000000;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000000;
+        end
+        NOP2_RETI:begin   //NOP3
+        reti_state_after=3'b000;
+        f_d_buffer_enable=1;
+        pc_enable=1;
+        flush=0;
+        jump_sel=2'b00;
+        EX_signals=14'b00000000000000;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000000;
+        end
+        endcase
+        end
+        else if(inst_before_call)  //call2
+        begin 
+        ret_state_after=3'b000;
+        reti_state_after=3'b000;
+        inter_state_after=2'b00;
+        f_d_buffer_enable=1;
+        pc_enable=1;
+        flush=0;
+        jump_sel=2'b00;
+        EX_signals=14'b00000001001001;
+        MEM_signals=7'b0111101;
+        WB_signals=6'b100000;
+        end
+        else 
+        begin  
+        ret_state_after=3'b000;
+        reti_state_after=3'b000;
+        inter_state_after=2'b00; 
+        f_d_buffer_enable=1;
+        pc_enable=1;
+        flush=0; 
         case (opcode)
-            NOP: begin 
-                flush=1'b0;
-                EX_signals=6'b0000000;
-                MEM_signals=4'b0000; 
-                WB_signals=3'b000;
-
+            NOP: begin                 //NOP
+        jump_sel=2'b00;
+        EX_signals=14'b00000000000000;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000000;
             end
-            NOT: begin 
-                flush=1'b0;
-                EX_signals=6'b0001101;
-                MEM_signals=4'b0000;
-                WB_signals=3'b101;
-               
+            SETC:begin        //SETC
+        jump_sel=2'b00;
+        EX_signals=14'b00001010000011;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000000;
             end
-            ADD: begin 
-                flush=1'b0;
-                EX_signals=6'b0010101;
-                MEM_signals=4'b0000;
-                WB_signals=3'b101;
+            CLRC:begin         //CLCR
+        jump_sel=2'b00;
+        EX_signals=14'b00001011000011;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000000;
             end
-            LDM: begin 
-                flush=1'b1;
-                EX_signals=6'b0000000;
-                MEM_signals=4'b1000;
-                WB_signals=3'b110;
-
+            NOT: begin        //NOT
+        jump_sel=2'b00;
+        EX_signals=14'b00000101000011;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000001;
             end
-            STD: begin
-                flush=1'b0;
-                EX_signals=6'b0000000;
-                MEM_signals=4'b0110;   //memRead(1), memWrite(1), memAddress(1), memData(1)
-                WB_signals=3'bxxx;
-                
+            INC:begin           //INC
+        jump_sel=2'b00;
+        EX_signals=14'b00000000000011;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000001;
+            end
+            DEC:begin      //DEC
+        jump_sel=2'b00;
+        EX_signals=14'b00000001000011;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000001;
+            end
+            PUSH:begin         //PUSH
+        jump_sel=2'b00;
+        EX_signals=14'b00000001000001;
+        MEM_signals=7'b0111001;
+        WB_signals=6'b100000;
+            end
+            POP:begin       //POP
+        jump_sel=2'b00;
+        EX_signals=14'b00000000000001;
+        MEM_signals=7'b1010000;
+        WB_signals=6'b101001;
+            end
+            ADD: begin             //ADD
+        jump_sel=2'b00;
+        EX_signals=14'b00000010000011;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000001;
+            end
+            SUB:begin        //SUB
+        jump_sel=2'b00;
+        EX_signals=14'b00000011000011;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000001;
+            end
+            AND:begin          //AND
+        jump_sel=2'b00;
+        EX_signals=14'b00000111000011;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000001;
+            end
+            OR:begin          //OR
+        jump_sel=2'b00;
+        EX_signals=14'b00000110000011;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000001;
+            end
+            MOV:begin          //MOV
+        jump_sel=2'b00;
+        EX_signals=14'b00000100000001;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000001;
+            end
+            SHL:begin             //SHL
+        jump_sel=2'b00;
+        EX_signals=14'b00001000000011;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000001;
+            end
+            SHR:begin           //SHR
+        jump_sel=2'b00;
+        EX_signals=14'b00001001000011;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000001;
+            end
+            LDM: begin               //LDM
+        flush=1;
+        jump_sel=2'b00;
+        EX_signals=14'b00000100100001;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000001;
+            end
+            LDD:begin          //LDD
+        jump_sel=2'b00;
+        EX_signals=14'b00000000000000;
+        MEM_signals=7'b1000000;
+        WB_signals=6'b001001;
+            end
+            STD: begin      //STD
+        jump_sel=2'b00;
+        EX_signals=14'b00000000000000;
+        MEM_signals=7'b0101000;
+        WB_signals=6'b000000;
+            end
+            JZ:begin              //JZ
+        jump_sel=2'b00;
+        EX_signals=14'b00100000000000;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000000;    
+            end
+            JN:begin                 //JN
+        jump_sel=2'b00;
+        EX_signals=14'b01001010000000;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000000;       
+            end
+            JC:begin                  //JC
+        jump_sel=2'b00;
+        EX_signals=14'b01101010000000;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000000;
+            end
+            JMP:begin              //JMP
+        flush=1;
+        jump_sel=2'b01;
+        EX_signals=14'b10001010000000;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000000;
+            end
+            OUT:begin             //OUT
+        jump_sel=2'b00;
+        EX_signals=14'b00001100000001;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000000;
+            end
+            IN:begin            //IN
+        jump_sel=2'b00;
+        EX_signals=14'b00000100010001;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000001;
+            end
+            CALL:begin         //CALL
+        f_d_buffer_enable=0;
+        jump_sel=2'b00;
+        EX_signals=14'b00010001001001;
+        MEM_signals=7'b0111110;
+        WB_signals=6'b100000;
             end
                 default: begin //NOP
-                flush=1'b0;
-                EX_signals=6'b0000100;
-                MEM_signals=4'b0000; 
-                WB_signals=3'b011;
+        jump_sel=2'b00;
+        EX_signals=14'b00000000000000;
+        MEM_signals=7'b0000000;
+        WB_signals=6'b000000;
             end
         endcase
+        end
     end
-   
 endmodule
--- assembler/Assembler.py
+++ assembler/Assembler.py
@@ -2,19 +2,19 @@ import re
 
 #read file and store in dectionary
 dictionary = {}
-with open("Dectionary.txt") as f:
+with open("Assembler/Dectionary.txt") as f:
     for line in f:
         (key, val) = line.split()
         dictionary[str(key)] = val
 
 #read code
 instructions = []
-with open("code.txt") as f:
+with open("Assembler/code.txt") as f:
     for line in f:
         line = line.upper()
         # ignore comments and empty lines
         line = line.replace('\t', '')   # for empty lines
-        line = line.strip()             # squezzing the line -> [k h a l e d] -> line.strip -> [khaled]
+        line = line.strip()             # squezzing the line --> [k h a l e d] -> line.strip -> [khaled]
         if(len(line) == 0 or line[0] == ';' or line[0] == '#' or line == "\n"):
             continue
 
@@ -30,16 +30,6 @@ with open("code.txt") as f:
         if(re.search(pattern, line)):
             line = line.split("\n")[0]
 
-        # split labels and instruction on diffrent lines
-        pattern = ':'
-        if(re.search(pattern,line)):
-            temp = line.split(':')[1]     #label:jmp R1
-            pattern = '.*[A-Z].*'
-            if(re.search(pattern, temp)):
-                instructions.append(line.split(':')[0]+':')
-                line = line.split(':')[1]
-                line=line.lstrip()
-        #save the instructions   
         instructions.append(line)
 
 
@@ -54,24 +44,17 @@ for i,instruction in enumerate(instructions):
     IR = "0000000000000000"
     twoOperands = False
     oneOperand = False
-    Branch = False
-
 
     #handling instructions
     inst = instructions[i].split(' ')[0]
+
     #Instruction OPcode
     code = dictionary[inst]
-    print(code)
 
-    twoOperands = True
-    # if(code[4:6] == "10"):
-    #     twoOperands = True
-        
-    # elif(code[4] == "0"):
-    #     oneOperand = True
-       
-    # elif(code[4:6] == "11"):
-    #     Branch= True
+    if (code[0:2] == "01"):
+        twoOperands = True           
+    else:
+        oneOperand = True
 
     IR = code + IR[6:]
     #cut the string after instruction
@@ -79,48 +62,42 @@ for i,instruction in enumerate(instructions):
     Operands = Operands.strip()
 
     # {* ========================= one operand & no operand ======================== *} 
-    if(oneOperand == True):
+    if(oneOperand):
         if(inst == "NOP" or inst == "SETC" or inst == "CLRC"):
             IR = IR[0:6] + "0000000000"
-            IRCodes.append( IR + "\n")
-            addressCounter = addressCounter + 1
-            continue
         else:
             IR = IR[0:6] + "000" + dictionary[Operands] + "0000" 
-            IRCodes.append( IR + "\n")
-            addressCounter = addressCounter + 1
-            continue
-        
-    # {* ================================ two operand ================================ *}  
-    if(twoOperands == True):
+    
+        IRCodes.append( IR + "\n")
+        addressCounter = addressCounter + 1
+        continue
+
+    # {* ================================ two operand =============================== *}  
+    if(twoOperands):
         src = Operands.split(',')[0]
         dst = Operands.split(',')[1]
-        #check if SHL & SHR & LDM 
-        if(inst == "SHL" or inst == "SHR" or inst == "LDM"):
+
+        #check for special cases 
+        if(inst == "LDM"):   
             IR = IR[0:6] + "000" + dictionary[src] + "0000"
             IRCodes.append( IR + "\n")
             addressCounter = addressCounter + 1
             IRCodes.append( bin(int(dst, 16))[2:].zfill(16) + "\n")  # get immediate value
             addressCounter = addressCounter + 1
             continue
-
+        elif(inst == "SHL" or inst == "SHR"):
+            shiftamount = bin(int(dst))[2:].zfill(4)
+            IR = IR[0:6]  + dictionary[src] + "000" + shiftamount
+            IRCodes.append( IR + "\n")
+            addressCounter = addressCounter + 1
         else:
             IR = IR[0:6] + dictionary[src] + dictionary[dst]+ '0000'
             IRCodes.append( IR +"\n")
             addressCounter = addressCounter + 1
             continue
  
-    # {* ================================== Branch ====================================== *}  
-    if(Branch == True):
-        if(inst == "RTI" or inst == "RET"):
-            IR = IR[0:6] + "0000000000"
-        else:
-            IR = IR[0:6] + "000" + dictionary[Operands] +"0000" 
-        IRCodes.append( IR +"\n")
-        addressCounter = addressCounter + 1
-
 
 #writing IR codes in output file
-outputFile = open("../Processor/1.Fetch/memory.txt","w")
+outputFile = open("Processor/1.Fetch/memory.txt","w")
 outputFile.writelines(IRCodes)
 outputFile.close()
